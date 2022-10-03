9ea0963fa0ac086ea2ef764fbc69c2ef03141036
saga0619 <june992@gmail.com>
2022-10-03 19:15:45 +0900

    launchmanager update

--- tocabi_launch_manager/src/launch_manager.py
+++ tocabi_launch_manager/src/launch_manager.py
@@ -3,12 +3,14 @@
 import rospy
 import roslaunch
 from std_msgs.msg import String
-from subprocess import Popen, PIPE
+import subprocess
+
+from subprocess import Popen, PIPE, TimeoutExpired
 from threading import Thread
 import sys
 from signal import SIGINT
 from datetime import datetime
-
+import os
 
 try:
     from queue import Queue, Empty
@@ -21,20 +23,29 @@ ON_POSIX = 'posix' in sys.builtin_module_names
 
 global starterSwitch
 global stopperSwitch
+global restarterSwitch
 global roslaunch_subprocess
+global res_subprocess
 
 
 starterSwitch = False
 stopperSwitch = False
+restarterSwitch = False
 
 
 def startercallback(data):
     global starterSwitch
+    global restarterSwitch
     global roslaunch_subprocess
     if data.data == "start_tocabi":
         pub_syslog.publish(String('Start Signal Received'))
         print("Start Signal Rcv",datetime.now().strftime("%H:%M:%S"))
         starterSwitch = True
+    if data.data == "restart_websocket":
+        pub_syslog.publish(String('Restart ROSBRIDGE_WEBSOCKET'))
+        print("restart websocket",datetime.now().strftime("%H:%M:%S"))
+        restarterSwitch = True
+
         
 
 def stoppercallback(data):
@@ -59,8 +70,24 @@ def enque_output(out,queue):
     for line in iter(out.readline, b''):
         # queue.put(line)
         pub_syslog.publish(String(str(line[:-1].decode())))
-        print(line[:-1].decode())
+        #print(line[:-1].decode())
+        # print('get line')
+        print(line.decode('utf-8').rstrip())
 
+    # print("end iter")
+    out.close()
+
+# Get stdout from subprocess and process it to queue
+def enque_output_err(out,queue): 
+    # print("start err iter")
+    for line in iter(out.readline(), b''):
+        # queue.put(line)
+        pub_syslog.publish(String(str(line[:-1].decode())))
+        #print(line[:-1].decode())
+        # print('get err line')
+        print(line.decode('utf-8').rstrip())
+
+    print("end web iter")
     out.close()
 
 
@@ -70,10 +97,14 @@ if __name__ == '__main__':
     output = Popen(['uname','-r'], stdout=PIPE).communicate()[0]
     print(output.decode())
 
-    rospy.init_node('tocabi_launcher', anonymous=True)
+    rospy.init_node('tocabi_launchmanager')
     rospy.Subscriber("/tocabi/starter",String,startercallback)
     rospy.Subscriber("/tocabi/stopper",String,stoppercallback)
+
     pub_syslog = rospy.Publisher("/tocabi/syslog",String, queue_size=10)
+    
+    pub_launchman = rospy.Publisher("/tocabi/launchman",String, queue_size=10)
+
     #uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     #roslaunch.configure_logging(uuid)
     #cli_args = ['tocabi_controller','realrobot.launch','log:=true','hand:=true']
@@ -85,10 +116,21 @@ if __name__ == '__main__':
 
     tick = 0
     q = None
+    q2 = None
     t = None
+    t2=None
+
+    launch_hearbeat = 0
+
     while not rospy.is_shutdown():
         rate.sleep()
 
+
+        launch_hearbeat = launch_hearbeat +1
+
+        if(launch_hearbeat == 300):
+            launch_hearbeat = 0
+            pub_launchman.publish(String("tocabi_launchman"))
         # if(rl_is_running == True):
         #     try: line = q.get_nowait()
         #     except Empty:
@@ -109,14 +151,27 @@ if __name__ == '__main__':
             rl_is_running = True
             roslaunch_subprocess = Popen(['roslaunch','tocabi_controller','realrobot.launch'],stdout=PIPE, stderr=PIPE)
             q=Queue()
+            # q2=Queue()
             t= Thread(target=enque_output, args=(roslaunch_subprocess.stdout,q))
+
+            # roslaunch_subprocess.stdout.
+            # t2= Thread(target=enque_output_err, args=(roslaunch_subprocess.stderr,q))
+
             t.daemon = True
+            # t2.daemon = True
             t.start()
+            # t2.start()
+
+
             #launch = roslaunch.parent.ROSLaunchParent(uuid, (roslaunch_file, roslaunch_args))
             #launch.start()
             starterSwitch = False
         
-        if(stopperSwitch == True):
+        if(stopperSwitch == True):            
+            try:
+                roslaunch_subprocess.communicate(timeout=1)
+            except TimeoutExpired:
+                print('communicate pass')
             rl_is_running = False
             print('sending sigint',datetime.now().strftime("%H:%M:%S"))
             roslaunch_subprocess.send_signal(SIGINT)
@@ -126,3 +181,11 @@ if __name__ == '__main__':
             # t.end()
             #launch.shutdown()
             stopperSwitch=False
+
+        
+        if(restarterSwitch == True):
+            restarterSwitch=False
+            print('restarting rosbridge_websocket')
+            res_subprocess =subprocess.run(['systemctl', 'restart', 'tocabi_webserver.service'])
+            
+            print(res_subprocess, 'restart complete rosbridge_websocket')
